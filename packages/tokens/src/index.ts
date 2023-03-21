be9b0eeb01dfa68cb02ea28024e71fcc0e79d993
Gustavo Theotonio <gustavotheotonio46@gmail.com>
2023-03-21 16:54:06 -0300

    stoped error

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,10 @@
+export * from './colors'
+
+export * from './space'
+export * from './radii'
+
+export * from './line-heights'
+
+export * from './fonts'
+export * from './font-weights'
+export * from './font-sizes'
